d9199a690861a626071b7aa37eeacf6886f7e6cd
Rodrigo Couto <r@rodg.co>
2022-08-24 14:44:33 -0300

    enhance zsh setup

--- setup.d/02-zsh.sh
+++ setup.d/02-zsh.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 chsh -s /bin/zsh
+mv ~/.oh-my-zsh ~/.oh-my-zsh-dotfiles
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp -r ~/.oh-my-zsh-dotfiles/* ~/.oh-my-zsh
+rm -rf ~/.oh-my-zsh-dotfiles
+rm ~/.zshrc
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+
