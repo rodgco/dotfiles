0bd16342f93c47107c93152d661db45618b680ec
Rodrigo Couto <r@rodg.co>
2023-04-22 08:53:18 -0300

    Fix setup

--- dotfiles/setup.d/00-dotfiles.sh
+++ dotfiles/setup.d/00-dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/.dotfiles/ --work-tree=$HOME'
-git clone --bare git:github.com:rodgco/dotfiles.git $HOME/dotfiles/.dotfiles
+git clone --bare git@github.com:rodgco/dotfiles.git $HOME/dotfiles/.dotfiles
 dotfiles remote set-url origin git@github.com:rodgco/dotfiles.git
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout
